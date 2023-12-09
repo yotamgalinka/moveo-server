7cb211d37d1c6c3bbd94983b56c18ca23e27d531
bylem <bylemaya@gmail.com>
2023-12-09 22:30:16 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,12 @@
 const mysql = require('mysql2');
 const codeBlocks = require('./extensions/code_blocks');
-// const mysql_password = require('./.env');
 require('dotenv').config();
 
 const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: process.env.mysql_password,
-    database: "moveo"
+    password: process.env.DB_PASSWORD,
+    database: "moveo",
 });
 
 connection.connect((err) => {
