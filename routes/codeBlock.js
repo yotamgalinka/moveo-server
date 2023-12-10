3f803ba38981545f2139429b635255cb38e0d460
bylem <bylemaya@gmail.com>
2023-12-10 09:31:51 +0200

    Deployment updates

--- routes/codeBlock.js
+++ routes/codeBlock.js
@@ -1,19 +1,36 @@
+// const express = require('express');
+// const codeBlocks = require('../extensions/code_blocks.js');
+// const db = require('../db.js');
+
+// const router = express.Router()
+
+// router.get("/", (req, res) => {
+//     const selectQuery = "SELECT * FROM moveo"
+//     db.query(selectQuery, (err, data) => {
+//         if(err) {
+//             return res.status(500).send(err)
+//         }
+//         return res.json(data)
+//     })
+// })
+
+// module.exports = router;
+
+
 const express = require('express');
-const codeBlocks = require('../extensions/code_blocks.js');
 const db = require('../db.js');
 
-const router = express.Router()
+const router = express.Router();
 
 router.get("/", (req, res) => {
-    const selectQuery = "SELECT * FROM moveo"
-    db.query(selectQuery, (err, data) => {
-        if(err) {
-            return res.status(500).send(err)
+    const selectQuery = "SELECT * FROM moveo";
+
+    db.all(selectQuery, (err, data) => {
+        if (err) {
+            return res.status(500).send(err);
         }
-        return res.json(data)
-    })
-})
+        return res.json(data);
+    });
+});
 
 module.exports = router;
-
-
