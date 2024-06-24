8eb0f7d7a296948185b97f16be52a5aeefa0fbe7
Yotam Galinka <yotamga98@gmail.com>
2024-06-24 20:18:18 +0300

    Update server for railway deployment 3

---
+++ test-db-connection.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME,
+  port: process.env.DB_PORT || 3306,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+  }
+  console.log('Connected to MySQL database');
+  connection.end(); // Close the connection after test
+});
